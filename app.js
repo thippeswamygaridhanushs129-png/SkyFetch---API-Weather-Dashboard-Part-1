6486141a52d455434a88b8545519469ddd2991e7
thippeswamygaridhanushs129-png <thippeswamygari.dhanush.s.129@kalvium.community>
2026-02-24 14:44:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Your OpenWeatherMap API Key
-const API_KEY = 'YOUR_API_KEY_HERE';  // Replace with your actual API key
+const API_KEY = 'b0bcb236e99b9e397f10f0813a4911ab';  // Replace with your actual API key
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 // Function to fetch weather data
